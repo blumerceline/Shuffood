1fd721f41dbfea4f2ae84b8edb2feb9a69d24a89
blumerceline <blumerceline@gmail.com>
2022-10-08 14:33:55 +1000

    background color

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -10,7 +10,7 @@ import {Link} from 'react-router-dom';
 const HomePage = () => (
     <Stack direction="column" sx={{backgroundColor: '#f5efd7', position: 'relative',alignItems:"center", justifyContent:"center", 
     flexWrap:"wrap"}}>
-        <Box sx={{height:{sx: 'auto', md:'92vh', xs:'92vh'}, px: {sx:0,md:2}}}>
+        <Box sx={{height:{sx: 'auto'}, px: {sx:0,md:2}}}>
             <Card>
                 <CardMedia image={process.env.PUBLIC_URL + "/images/logo.JPG"} alt={'recipepic'} sx={{width: 358, height: 180}}/>
                         <CardContent sx={{ backgroundColor:'#white', height:'106px'}}>
