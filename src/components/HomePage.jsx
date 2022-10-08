b4344c2bd4990300da0f9a3ef906cc2abf9b01be
blumerceline <blumerceline@gmail.com>
2022-10-08 12:51:55 +1000

    first commit

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+import {Box, Stack, Typography} from '@mui/material'
+import {SideBar, Videos} from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+
+
+const HomePage = () => (
+    <Box sx={{height:{sx: 'auto', md:'92vh'}, backgroundColor: '#25242a', px: {sx:0,md:2}}}>
+
+
+    </Box>
+)
+
+export default HomePage
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Stack} from '@mui/material';
+import {Link} from 'react-router-dom';
+
+const Navbar = () => (
+  <Stack direction="row" alignItems="center" p={3} sx={{position:'sticky',background:'#f5efd7', top:0, justifyContent:'center'}}>
+    <Link to="/" style={{display:'flex', alignItems:'center'}}>
+        <img src={process.env.PUBLIC_URL + "/images/logo.JPG"} alt="logo" height={100}/>
+      </Link>
+    
+  </Stack>
+)
+
+export default Navbar
