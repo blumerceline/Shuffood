a7fcd517eb29f3587c51093cbc38692ababa0c17
blumerceline <blumerceline@gmail.com>
2022-10-09 14:42:29 +1000

    efe

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 const Navbar = () => (
   <Stack direction="row" position="sticky" alignItems="center" p={3} sx={{position:'sticky',background:'#f5efd7', top:0, justifyContent:'center'}}>
     <Link to="/" style={{display:'flex', alignItems:'center'}}>
-        <img src={process.env.PUBLIC_URL + "/images/logo.JPG"} alt="logo" height={100}/>
+        <img src={process.env.PUBLIC_URL + "/images/logo.JPG"} alt="logo" height={250}/>
       </Link>
     
   </Stack>
